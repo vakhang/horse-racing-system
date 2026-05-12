75cdb031184b7d7e1715e6e5c32928a80e851f94
vakhang <168823569+vakhang@users.noreply.github.com>
2026-05-12 09:46:39 +0700

    init: setup thanh cong base Spring Boot va ket noi PostgreSQL

---
+++ backend/src/main/java/com/swp/horseracing/HorseracingApplication.java
@@ -0,0 +1,13 @@
+package com.swp.horseracing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorseracingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HorseracingApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/swp/horseracing/HorseracingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swp.horseracing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HorseracingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
