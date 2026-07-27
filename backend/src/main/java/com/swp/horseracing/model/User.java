253b4484d578c4686ab3f68b25220be95d2626b5
xuankhang2412 <xuankhang2412@gmail.com>
2026-07-27 22:10:29 +0700

    chore: remove unused imports in User.java

--- backend/src/main/java/com/swp/horseracing/model/User.java
+++ backend/src/main/java/com/swp/horseracing/model/User.java
@@ -3,8 +3,7 @@ package com.swp.horseracing.model;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.JdbcTypeCode;
-import org.hibernate.type.SqlTypes;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
