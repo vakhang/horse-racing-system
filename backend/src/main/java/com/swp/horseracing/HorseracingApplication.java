d091e26fd5227b892f75b9d295aefbe17c659d70
xuankhang2412 <xuankhang2412@gmail.com>
2026-07-29 17:56:26 +0700

    chore: add dummy attachments for test users

--- backend/src/main/java/com/swp/horseracing/HorseracingApplication.java
+++ backend/src/main/java/com/swp/horseracing/HorseracingApplication.java
@@ -16,7 +16,44 @@ public class HorseracingApplication {
 	@org.springframework.beans.factory.annotation.Autowired
 	private com.swp.horseracing.repository.WalletRepository walletRepository;
 
-	@org.springframework.beans.factory.annotation.Autowired
-	private com.swp.horseracing.repository.TransactionHistoryRepository transactionHistoryRepository;
+	@jakarta.annotation.PostConstruct
+	public void fillMissingAttachments() {
+		java.util.List<com.swp.horseracing.model.User> users = userRepository.findAll();
+		String dummyUrl = "https://res.cloudinary.com/demo/image/upload/v1312461204/sample.jpg";
+		for (com.swp.horseracing.model.User u : users) {
+			java.util.List<com.swp.horseracing.model.UserAttachment> attachments = u.getAttachments();
+			if (attachments == null) attachments = new java.util.ArrayList<>();
+			
+			boolean hasIdCard = attachments.stream().anyMatch(a -> a.getDocType() == com.swp.horseracing.model.UserDocType.ID_CARD);
+			boolean hasCert = attachments.stream().anyMatch(a -> a.getDocType() == com.swp.horseracing.model.UserDocType.CERTIFICATE);
+			boolean hasHealth = attachments.stream().anyMatch(a -> a.getDocType() == com.swp.horseracing.model.UserDocType.HEALTH_CHECK);
+
+			boolean added = false;
+			if (!hasIdCard) {
+				attachments.add(com.swp.horseracing.model.UserAttachment.builder().user(u).docType(com.swp.horseracing.model.UserDocType.ID_CARD).fileUrl(dummyUrl).build());
+				added = true;
+			}
+
+			if (u.getRole() == com.swp.horseracing.model.RoleEnum.JOCKEY) {
+				if (!hasCert) {
+					attachments.add(com.swp.horseracing.model.UserAttachment.builder().user(u).docType(com.swp.horseracing.model.UserDocType.CERTIFICATE).fileUrl(dummyUrl).build());
+					added = true;
+				}
+				if (!hasHealth) {
+					attachments.add(com.swp.horseracing.model.UserAttachment.builder().user(u).docType(com.swp.horseracing.model.UserDocType.HEALTH_CHECK).fileUrl(dummyUrl).build());
+					added = true;
+				}
+			} else if (u.getRole() == com.swp.horseracing.model.RoleEnum.REFEREE) {
+				if (!hasCert) {
+					attachments.add(com.swp.horseracing.model.UserAttachment.builder().user(u).docType(com.swp.horseracing.model.UserDocType.CERTIFICATE).fileUrl(dummyUrl).build());
+					added = true;
+				}
+			}
+
+			if (added) {
+				userRepository.save(u);
+			}
+		}
+	}
 
 }
